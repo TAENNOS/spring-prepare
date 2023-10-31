a4516303e9c67ab93f134e0fdef47da91b09a976
An <taennos1017@naver.com>
2023-10-31 20:07:30 +0900

    springPrac

---
+++ src/test/java/com/springprac/springprepare/SpringPrepareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springprac.springprepare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPrepareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
