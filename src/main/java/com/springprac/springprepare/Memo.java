e518d59018b5f6612ad3487cd71e12591232d089
An <taennos1017@naver.com>
2023-11-01 19:46:56 +0900

    커밋

---
+++ src/main/java/com/springprac/springprepare/Memo.java
@@ -0,0 +1,11 @@
+package com.springprac.springprepare;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Memo {
+    private String username;
+    private String contents;
+}
